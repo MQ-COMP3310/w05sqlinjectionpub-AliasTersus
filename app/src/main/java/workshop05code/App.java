864747466e1d63a2b63a8968f29cb1976920391a
AliasTersus <morganmears0@gmail.com>
2025-03-28 09:33:18 +1100

    commit after jdk version changes and gradle version update

--- app/src/main/java/workshop05code/App.java
+++ app/src/main/java/workshop05code/App.java
@@ -75,7 +75,12 @@ public class App {
 
             while (!guess.equals("q")) {
                 System.out.println("You've guessed '" + guess+"'.");
-
+                if(!guess.matches("^[a-z]{4}$")){
+                    System.out.println("You have used invalid characters! Please try again.");
+                    System.out.print("Enter a 4 letter word for a guess or q to quit: " );
+                    guess = scanner.nextLine();
+                }
+                
                 if (wordleDatabaseConnection.isValidWord(guess)) { 
                     System.out.println("Success! It is in the the list.\n");
                 }else{
@@ -84,6 +89,7 @@ public class App {
 
                 System.out.print("Enter a 4 letter word for a guess or q to quit: " );
                 guess = scanner.nextLine();
+            
             }
         } catch (NoSuchElementException | IllegalStateException e) {
             e.printStackTrace();
